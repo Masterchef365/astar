d830e00ef95a7c820a2ea01afa52800f7dd921d5
Masterchef365 <duncan.freeman1@gmail.com>
2022-02-22 00:32:58 -0800

    Paths!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use astar::djikstra::djikstra;
+use rand::prelude::*;
 use std::fs::File;
 use std::io::BufWriter;
 
@@ -10,49 +11,62 @@ fn main() {
     let mut obstacles = vec![false; width * height];
     let image_path = "out.png";
 
-    for y in 10..=90 {
-        obstacles[bounds(40, y, width, height).unwrap()] = true;
+    let n_paths = 50;
+
+    let mut rng = rand::thread_rng();
+
+    fn random_pos(mut rng: impl Rng, width: usize, height: usize) -> Coord {
+        (
+            rng.gen_range(0..width as isize),
+            rng.gen_range(0..height as isize),
+        )
     }
 
-    for y in 100..=300 {
-        obstacles[bounds(100, y, width, height).unwrap()] = true;
-    }
+    let goals: Vec<(Coord, Coord)> = (0..n_paths)
+        .map(|_| {
+            (
+                random_pos(&mut rng, width, height),
+                random_pos(&mut rng, width, height),
+            )
+        })
+        .collect();
 
-    //let begin = (width as isize / 3, height as isize / 3);
-    let begin = (10, 10);
-    let end = (2 * width as isize / 3, 2 * height as isize / 3);
-
-    // Neighbors
-    let neighbors = |(x, y)| {
-        four_directions((x, y))
-        //eight_directions((x, y))
-            .into_iter()
-            .filter(|&(x, y)| match bounds(x, y, width, height) {
-                Some(idx) => !obstacles[idx],
-                None => false,
-            })
-    };
-
-    let cost = |a, b| 1 + heuristic(b, a, end);
-
-    // Calculate path
-    let path = djikstra(cost, neighbors, begin, end);
-    let path = path.expect("No path!");
-
-    // Write image
+    // Write to image
     let mut output_image = vec![0u8; width * height * 3];
 
-    output_image
-        .chunks_exact_mut(3)
-        .zip(obstacles)
-        .filter(|(_, b)| *b)
-        .for_each(|(u, _)| u[1] = 0xff);
+    for (goal_idx, (begin, end)) in goals.into_iter().enumerate() {
+        let path_color = index_color(goal_idx);
 
-    for (x, y) in path {
-        if let Some(idx) = bounds(x, y, width, height) {
-            output_image[idx * 3 + 0] = 0xff;
-            //output_image[idx * 3 + 1] = 0xff;
-            //output_image[idx * 3 + 2] = 0xff;
+        // Neighbors
+        let neighbors = |(x, y)| {
+            four_directions((x, y))
+                //eight_directions((x, y))
+                .into_iter()
+                .filter(|&(x, y)| match bounds(x, y, width, height) {
+                    Some(idx) => !obstacles[idx],
+                    None => false,
+                })
+        };
+
+        let cost = |a, b| 1 + heuristic(b, a, end);
+
+        // Calculate path
+        if let Some(path) = djikstra(cost, neighbors, begin, end) {
+
+            /*
+            output_image
+                .chunks_exact_mut(3)
+                .zip(obstacles)
+                .filter(|(_, b)| *b)
+                .for_each(|(u, _)| u[1] = 0xff);
+            */
+
+            for (x, y) in path {
+                if let Some(idx) = bounds(x, y, width, height) {
+                    obstacles[idx] = true;
+                    output_image[idx * 3..][..3].copy_from_slice(&path_color);
+                }
+            }
         }
     }
 
@@ -102,4 +116,9 @@ fn eight_directions((x, y): Coord) -> [Coord; 8] {
         (x, y + 1),
         (x - 1, y + 1),
     ]
+}
+
+fn index_color(idx: usize) -> [u8; 3] {
+    let colors = [[0xFF, 0xEC, 0x04], [0x38, 0xC6, 0xDB], [0xDB, 0x38, 0x83]];
+    colors[idx % colors.len()]
 }
