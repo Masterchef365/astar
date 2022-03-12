30e72830f2bb23413cd38b5381d9896a46ab5ee7
Duncan Freeman <dfreeman@lucidyne.com>
2022-03-11 16:33:48 -0800

    3D output!

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
+use anyhow::Result;
 use astar::djikstra::djikstra;
+use idek::prelude::*;
 use rand::prelude::*;
 use std::fs::File;
 use std::io::BufWriter;
-use structopt::StructOpt;
 use std::path::PathBuf;
-use anyhow::Result;
+use structopt::StructOpt;
 
-type Coord = (isize, isize);
+type Coord = (isize, isize, isize);
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    #[structopt(short, long, default_value = "1920")]
+    #[structopt(short, long, default_value = "100")]
     width: usize,
 
-    #[structopt(short, long, default_value = "1080")]
+    #[structopt(short, long, default_value = "100")]
     height: usize,
 
-    /// Input RGB PNG image, non-#000000 pixels will be considered obstacles.
-    /// Overrides width and height parameters.
-    #[structopt(short = "b", long)]
-    obstacles: Option<PathBuf>,
-
-    /// Output image path
-    #[structopt(short, long, default_value = "out.png")]
-    out_path: PathBuf,
+    #[structopt(short, long, default_value = "100")]
+    length: usize,
 
     /// Number of paths
-    #[structopt(short, long, default_value = "40000")]
+    #[structopt(short, long, default_value = "400")]
     n_paths: usize,
 
     /// Maximum number of iterations
@@ -37,39 +32,20 @@ struct Opt {
     #[structopt(short, long)]
     seed: Option<u64>,
 
-    /// Lines avoid one another in this radius
-    #[structopt(short = "r", long, default_value = "10")]
-    avoid_radius: isize,
-
-    /// Minimum path start/end distance
-    #[structopt(short = "d", long, default_value = "5000")]
-    min_path_dist: isize
+    // /// Lines avoid one another in this radius
+    // #[structopt(short = "r", long, default_value = "10")]
+    // avoid_radius: isize,
+    // /// Minimum path start/end distance
+    // #[structopt(short = "d", long, default_value = "5000")]
+    // min_path_dist: isize,
 }
 
 fn main() -> Result<()> {
-    let args = Opt::from_args();
+    let mut args = Opt::from_args();
+    args.height = args.width;
+    args.length = args.width;
 
-    // Decode obstacles, if any
-    let (width, height, mut obstacles) = match args.obstacles {
-        Some(obstacle_path) => {
-            let decoder = png::Decoder::new(File::open(obstacle_path)?);
-            let mut reader = decoder.read_info()?;
-            let mut buf = vec![0; reader.output_buffer_size()];
-            let info = reader.next_frame(&mut buf)?;
-            assert!(info.color_type == png::ColorType::Rgb);
-            assert!(info.bit_depth == png::BitDepth::Eight);
-            let input_bytes = &buf[..info.buffer_size()];
-
-            let width = info.width as usize;
-            let height = info.height as usize;
-            assert_eq!(input_bytes.len(), width * height * 3);
-            let mut obstacles = vec![false; width * height];
-            obstacles.iter_mut().zip(input_bytes.chunks_exact(3)).for_each(|(ob, inp)| *ob = inp != &[0; 3]);
-
-            (width, height, obstacles)
-        },
-        None => (args.width, args.height, vec![false; args.width * args.height]),
-    };
+    let mut obstacles = vec![false; args.width * args.height * args.length];
 
     let seed = args.seed.unwrap_or_else(|| rand::thread_rng().gen());
     let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
@@ -78,78 +54,131 @@ fn main() -> Result<()> {
     let goals: Vec<(Coord, Coord)> = (0..args.n_paths)
         .map(|_| {
             (
-                random_pos(&mut rng, width, height),
-                random_pos(&mut rng, width, height),
+                random_pos(&mut rng, args.width, args.height, args.length),
+                random_pos(&mut rng, args.width, args.height, args.length),
             )
         })
-        .filter(|(begin, end)| dist_sq(*begin, *end) >= args.min_path_dist)
+        //.filter(|(begin, end)| dist_sq(*begin, *end) >= args.min_path_dist)
         .take(args.n_paths)
         .collect();
 
-    // Write to image
-    let mut output_image = vec![0u8; width * height * 3];
+    let mut gb = GraphicsBuilder::new();
 
     for (goal_idx, (begin, end)) in goals.iter().enumerate() {
         if goal_idx % 1_000 == 0 {
             dbg!(goal_idx);
         }
 
-        let path_color = index_color(goal_idx);
+        let path_color = index_color(goal_idx).map(|v| v as f32 / 256.0);
 
         // Neighbors
-        let neighbors = |(x, y)| {
-            four_directions((x, y))
-                .into_iter()
-                .filter(|&(x, y)| match bounds(x, y, width, height) {
+        let neighbors = |coord| {
+            neighbors(coord).into_iter().filter(|&neigh| {
+                match bounds(neigh, args.width, args.height, args.length) {
                     Some(idx) => !obstacles[idx],
                     None => false,
-                })
+                }
+            })
         };
 
         let cost = |a, b| 1 + heuristic(b, a, *end);
 
         // Calculate path
         if let Some(path) = djikstra(cost, neighbors, *begin, *end, args.max_iters) {
-            for (x, y) in path {
-                if let Some(idx) = bounds(x, y, width, height) {
+            for &coord in &path {
+                if let Some(idx) = bounds(coord, args.width, args.height, args.length) {
                     obstacles[idx] = true;
-                    output_image[idx * 3..][..3].copy_from_slice(&path_color);
-                    for (x, y) in circle((x, y), args.avoid_radius) {
-                        if let Some(idx) = bounds(x, y, width, height) {
-                            obstacles[idx] = true;
-                        }
-                    }
                 }
+                //output_image[idx * 3..][..3].copy_from_slice(&path_color);
+                //for (x, y) in circle((x, y), args.avoid_radius) {
+                /*if let Some(idx) = bounds(coord, args.width, args.height, args.length) {
+                    obstacles[idx] = true;
+                }*/
+                //}
+            }
+
+            let maxdim = args.width.max(args.height.max(args.length));
+            for pair in path.windows(2) {
+                let vert = |(x, y, z): Coord| {
+                    Vertex::new(
+                        [x, y, z]
+                            .map(|v| v as f32 / maxdim as f32)
+                            .map(|v| v * 2. - 1.),
+                        path_color,
+                    )
+                };
+                let begin = gb.push_vertex(vert(pair[0]));
+                let end = gb.push_vertex(vert(pair[1]));
+                gb.push_indices(&[begin, end]);
             }
         }
     }
 
-    // For reading and opening files
-    let file = File::create(args.out_path)?;
-    let ref mut w = BufWriter::new(file);
+    dbg!(gb.vertices.len());
+    dbg!(gb.indices.len());
 
-    let mut encoder = png::Encoder::new(w, width as _, height as _); // Width is 2 pixels and height is 1.
-    encoder.set_color(png::ColorType::Rgb);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header()?;
+    launch::<_, Visualizer>(Settings::default().args(gb))
+}
 
-    writer.write_image_data(&output_image)?; // Save
-    Ok(())
+struct Visualizer {
+    verts: VertexBuffer,
+    indices: IndexBuffer,
+    shader: Shader,
+    camera: MultiPlatformCamera,
+}
+
+impl App<GraphicsBuilder> for Visualizer {
+    fn init(ctx: &mut Context, platform: &mut Platform, gb: GraphicsBuilder) -> Result<Self> {
+        let verts = ctx.vertices(&gb.vertices, false)?;
+        let indices = ctx.indices(&gb.indices, false)?;
+        let shader = ctx.shader(
+            DEFAULT_VERTEX_SHADER,
+            DEFAULT_FRAGMENT_SHADER,
+            Primitive::Lines,
+        )?;
+        Ok(Self {
+            verts,
+            indices,
+            shader,
+            camera: MultiPlatformCamera::new(platform),
+        })
+    }
+
+    fn frame(&mut self, _ctx: &mut Context, _: &mut Platform) -> Result<Vec<DrawCmd>> {
+        Ok(vec![DrawCmd::new(self.verts)
+            .indices(self.indices)
+            .shader(self.shader)])
+    }
+
+    fn event(
+        &mut self,
+        ctx: &mut Context,
+        platform: &mut Platform,
+        mut event: Event,
+    ) -> Result<()> {
+        if self.camera.handle_event(&mut event) {
+            ctx.set_camera_prefix(self.camera.get_prefix())
+        }
+        idek::close_when_asked(platform, &event);
+        Ok(())
+    }
 }
 
 /// Generate a random coordinate
-fn random_pos(mut rng: impl Rng, width: usize, height: usize) -> Coord {
+fn random_pos(mut rng: impl Rng, width: usize, height: usize, length: usize) -> Coord {
     (
         rng.gen_range(0..width as isize),
         rng.gen_range(0..height as isize),
+        rng.gen_range(0..length as isize),
     )
 }
 
 /// If the given coordinate is in bounds, return it's index within an image with the given
 /// dimensions
-fn bounds(x: isize, y: isize, width: usize, height: usize) -> Option<usize> {
-    (x >= 0 && y >= 0 && x < width as isize && y < height as isize)
-        .then(|| x as usize + y as usize * width)
+fn bounds((x, y, z): Coord, width: usize, height: usize, length: usize) -> Option<usize> {
+    let check = |v: isize, bound: usize| v >= 0 && v < bound as isize;
+    (check(x, width) && check(y, height) && check(z, length))
+        .then(|| x as usize + y as usize * width + z as usize * width * length)
 }
 
 fn heuristic(a: Coord, b: Coord, goal: Coord) -> isize {
@@ -157,28 +186,37 @@ fn heuristic(a: Coord, b: Coord, goal: Coord) -> isize {
 }
 
 /// Squared distance
-fn dist_sq((x, y): Coord, (goal_x, goal_y): Coord) -> isize {
+fn dist_sq((x, y, z): Coord, (goal_x, goal_y, goal_z): Coord) -> isize {
     let dx = x - goal_x;
     let dy = y - goal_y;
+    let dz = z - goal_z;
 
-    dx * dx + dy * dy
+    dx * dx + dy * dy + dz * dz
 }
 
 /// Up, down, left, right
-fn four_directions((x, y): Coord) -> [Coord; 4] {
-    [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+fn neighbors((x, y, z): Coord) -> [Coord; 6] {
+    [
+        (x - 1, y, z),
+        (x + 1, y, z),
+        (x, y - 1, z),
+        (x, y + 1, z),
+        (x, y, z - 1),
+        (x, y, z + 1),
+    ]
 }
 
-/// Filled circle with the given center and radius 
-fn circle((x, y): Coord, r: isize) -> impl Iterator<Item = Coord> {
-    // TODO: Make this faster!
-    (-r..=r)
-        .map(move |dx| (-r..=r).map(move |dy| (dx, dy)))
-        .flatten()
-        .filter(move |(dx, dy)| dx * dx + dy * dy < r * r)
-        .map(move |(dx, dy)| (x + dx, y + dy))
+/// Filled circle with the given center and radius
+/*
+   fn circle((x, y): Coord, r: isize) -> impl Iterator<Item = Coord> {
+// TODO: Make this faster!
+(-r..=r)
+.map(move |dx| (-r..=r).map(move |dy| (dx, dy)))
+.flatten()
+.filter(move |(dx, dy)| dx * dx + dy * dy < r * r)
+.map(move |(dx, dy)| (x + dx, y + dy))
 }
-
+*/
 // TODO: Make this a parameter!!
 /// Color LUT
 fn index_color(idx: usize) -> [u8; 3] {
@@ -188,4 +226,38 @@ fn index_color(idx: usize) -> [u8; 3] {
     let diff: i32 = 30;
     let mut component = |idx: usize| (base[idx] + rng.gen_range(-diff..=diff)).clamp(0, 255) as u8;
     [component(0), component(1), component(2)]
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct GraphicsBuilder {
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
+}
+
+impl GraphicsBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Push a Vertex and return it's index
+    pub fn push_vertex(&mut self, v: Vertex) -> u32 {
+        let idx: u32 = self
+            .vertices
+            .len()
+            .try_into()
+            .expect("Vertex limit exceeded");
+        self.vertices.push(v);
+        idx
+    }
+
+    /// Push an index
+    pub fn push_indices(&mut self, idx: &[u32]) {
+        self.indices.extend_from_slice(idx);
+    }
+
+    /// Erase all content
+    pub fn clear(&mut self) {
+        self.indices.clear();
+        self.vertices.clear();
+    }
 }
